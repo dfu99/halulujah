406eca8edd91b15a46afffd65a0ab7b36348927d
dfu <26264874+dfu99@users.noreply.github.com>
2025-07-28 14:24:48 -0400

    Modified finetune slurm script for deepspeed

--- bash/finetune.sh
+++ bash/finetune.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J phi3_ft
 #SBATCH -A gts-yke8
-#SBATCH -N1 --ntasks=1 --gres=gpu:A100:1
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:A100:2
 #SBATCH -C A100-80GB
-#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=64G
 #SBATCH --output=/storage/home/hcoda1/6/dfu71/scratch/halulujah/logs/slurm-%j.out
@@ -18,11 +18,15 @@ cd ~/scratch/halulujah
 # Activate virtual environment
 source venv_ft/bin/activate
 
-export RANK=0
-export WORLD_SIZE=$SLURM_NTASKS
-export LOCAL_RANK=0
-export MASTER_PORT=12355
-export MASTER_ADDR="localhost"
+## For single GPU fine tuning
+#export RANK=0
+#export WORLD_SIZE=$SLURM_NTASKS
+#export LOCAL_RANK=0
+#export MASTER_PORT=12355
+#export MASTER_ADDR="localhost"
 
 # Run Python script
-srun python src/finetune/finetune.py
+#srun python src/finetune/finetune.py
+
+## Multiple GPUs with DeepSpeed
+deepspeed --num_gpus=2 src/finetune/finetune.py
