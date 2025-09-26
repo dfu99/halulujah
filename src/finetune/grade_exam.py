ffcc640786f9cad6826dad500152b25d8445faa9
DAN_FU <26264874+dfu99@users.noreply.github.com>
2025-09-25 22:40:53 -0400

    Filtering logic

--- src/finetune/grade_exam.py
+++ src/finetune/grade_exam.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     exam_files = os.listdir(EXAM_FILES_PATH)
     graded_exam_files = os.listdir(GRADED_EXAM_FILES_PATH) if os.path.exists(GRADED_EXAM_FILES_PATH) else []
     # Filter out already graded files
-    exam_files = [f for f in exam_files if os.path.basename(f)+"_graded.json" not in graded_exam_files or not f.endswith(".out")]
+    exam_files = [f for f in exam_files if os.path.basename(f)+"_graded.json" not in graded_exam_files and not f.endswith(".out")]
 
     # Print the files to be graded
     print(f"Number of already graded files: {len(graded_exam_files)}")
