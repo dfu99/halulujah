45f59edeb0d4b12cddbf142021f908508754fb48
dan <26264874+dfu99@users.noreply.github.com>
2025-10-28 23:56:17 -0400

    Add graded exam from -ex fine-tuned model

--- src/finetune/grade_exam.py
+++ src/finetune/grade_exam.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     client = OpenAI(api_key=openai_api_key)
 
     # Exam files path
-    EXAM_FILES_PATH = "archive/Phi3.5-Base_EXAM_20250924/"
+    EXAM_FILES_PATH = "archive/EGNIVIA-ex_EXAM_20251024/"
     # Graded exam files path
     GRADED_EXAM_FILES_PATH = "exam_results/"
     # List all exam files
